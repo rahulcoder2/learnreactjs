b821e1b4d6fcbfa85750bfb5eb5b2f2bb1b76ad8
rahulcoder2 <rahulchaudharycsit077@gmail.com>
2023-11-06 08:52:07 +0545

    Todo using Redux-toolkit

---
+++ 11Redux-toolkitTodo/src/App.jsx
@@ -0,0 +1,19 @@
+
+import AddTodo from "./components/AddTodo";
+import Todos from "./components/Todos";
+
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>Redux ToolKit learning</h1>
+        <AddTodo/>
+        <Todos/>
+      </div>
+    </>
+  );
+}
+
+export default App;
